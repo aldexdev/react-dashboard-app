44bd8bb79d7572e7c1a947c675c5f2191342e66b
Alejandro Delgado Ontana <alejandro@Alejandros-MacBook-Pro.local>
2023-03-09 13:00:09 +0100

    implementing charts

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  const date = new Date().getFullYear();
+  return (
+    <div className="mt-24">
+      <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+        ©{date} All rights reserved by Shoppy.com
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
